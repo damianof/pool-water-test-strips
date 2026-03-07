f35cb17354ea2043ea522cce2c00feb53b9bb949
damianof <damianof@users.noreply.github.com>
2026-03-07 09:06:23 -0500

    vite config

--- vite.config.ts
+++ vite.config.ts
@@ -3,5 +3,6 @@ import vue from '@vitejs/plugin-vue'
 import tailwindcss from '@tailwindcss/vite'
 
 export default defineConfig({
+  base: 'damianof/pool-water-test-strips',
   plugins: [vue(), tailwindcss()],
 })
